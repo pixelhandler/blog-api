73d48fb118dcfaf61a3023e451a0ae12b204096e
Bill Heaton <bill@mediaartslab.com>
2015-05-03 17:39:59 -0700

    Setup prod deployment with capistrano
    - Include an example seeds file

--- db/seeds.example.rb
+++ db/seeds.example.rb
@@ -1,19 +1,20 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-author = Author.create({ name: 'admin', email: 'admin@me.com' })
+# Create an author of the post resources, an author may have a related user record
+author = Author.create({ name: 'pixelhandler', email: 'pixelhandler@gmail.com' })
 
-Post.create({
-  body: 'First post body',
-  excerpt: 'First post excerpt',
-  slug: 'first-post-slug',
-  title: 'First post title',
-  date: Date.new(2015, 5, 4),
-  author: author
-})
-
+# Create a user, w/ credentials may login & use Auth header to create, edit & remove resources
 User.create({
-  username: 'admin123',
-  password: 'admin456',
+  username: 'xxxxxxxx',
+  password: 'yyyyyyyy',
   author: author
 })
+
+# Load Post records from a JSON file, and assign the author to each
+json = ActiveSupport::JSON.decode(File.read('db/seeds/posts.json'))
+json.each do |post|
+  record = Post.new post
+  record.author = author;
+  record.save!
+end
