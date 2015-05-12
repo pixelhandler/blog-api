7a93cfd0a0bae439fbbbe99041a44957bbfd153f
Bill Heaton <bill@mediaartslab.com>
2015-05-11 22:43:16 -0700

    Use projected attributes hash with comment resource

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -5,6 +5,7 @@ class Api::AuthController < ApplicationController
   def authenticate
     user = User.find_by_credentials(params[:username], params[:password])
     if user
+      @current_user = user
       render json: { auth_token: user.generate_auth_token }
     else
       render json: { error: 'Invalid username or password' }, status: :unauthorized
@@ -13,17 +14,18 @@ class Api::AuthController < ApplicationController
 
   def authenticate_commenter
     commenter = Commenter.find_by_email(params[:email])
-    unless commenter
+    unless commenter.present?
       commenter = Commenter.create!(name: params[:username], email: params[:email])
     end
-    if commenter
+    if commenter.present?
+      @current_user = commenter
+      render json: { auth_token: commenter.generate_auth_token }
       render json: { auth_token: commenter.generate_auth_token, commenter_id: commenter.id }
     else
       render json: { error: 'Invalid name or email' }, status: :unauthorized
     end
-  rescue => e
-    byebug
-    render json: { error: 'Could not create or authenticate user'}, status: :unauthorized
+  rescue Exception => e
+    render json: { error: e.message }, status: :unauthorized
   end
 
   private
--- app/resources/api/v1/comment_resource.rb
+++ app/resources/api/v1/comment_resource.rb
@@ -1,7 +1,40 @@
 require 'jsonapi/resource'
 
 class Api::V1::CommentResource < JSONAPI::Resource
-  attributes :id, :body, :approved, :created_at
+  attributes :id, :body, :created_at
+  attribute :approved
   has_one :commenter
   has_one :post
+
+  def approved
+    if user_is_authorized or comment_owned_by_commenter?(@model)
+      return @model.approved
+    end
+  end
+
+  def fetchable_fields
+    user_is_known ? super : super - [:approved]
+  end
+
+  private
+
+    def current_user
+      context.respond_to?(:current_user) ? context.current_user : nil
+    end
+
+    def user_is_commenter
+      current_user.present? and current_user.is_a? Commenter
+    end
+
+    def user_is_authorized
+      current_user.present? and current_user.is_a? User
+    end
+
+    def user_is_known
+      user_is_commenter or user_is_authorized
+    end
+
+    def comment_owned_by_commenter?(model)
+      user_is_commenter and model.commenter.id == current_user.id
+    end
 end
