f6d42c09cdd20be3b3761294dc1bc1d3339f650f
Bill Heaton <bill@mediaartslab.com>
2015-12-29 14:19:50 -0800

    Increase default page size to 25 from 5

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -8,6 +8,6 @@ JSONAPI.configure do |config|
   # built in paginators are :none, :offset, :cursor, :paged
   config.default_paginator = :offset
 
-  config.default_page_size = 5
+  config.default_page_size = 25
   config.maximum_page_size = 100
 end
